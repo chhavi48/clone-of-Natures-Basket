92198f8e24dc84f06d579015bb3cd64db216191b
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-03 22:31:14 +0530

    slider

--- natures/src/App.js
+++ natures/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "bootstrap/dist/js/bootstrap.min.js";
 import Slider from './Components/Slider';
 function App() {
   return (
