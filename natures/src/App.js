373402645be5918292409513e3a92b4ec0ec894f
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-03 19:41:33 +0530

    slider

--- natures/src/App.js
+++ natures/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-  <h1>HEADER</h1>
+    <h1>hello</h1>
     </div>
   );
 }
---
+++ natures/src/Components/Slider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-sm-12'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Slider
