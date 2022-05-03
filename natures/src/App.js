91b7b3cee0359e00e3fee16bc93317968e53da1d
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-03 19:51:28 +0530

    new

--- natures/src/App.js
+++ natures/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Slider from './Components/Slider';
 function App() {
   return (
     <div className="App">
     <h1>hello</h1>
+    <Slider/>
     </div>
   );
 }
