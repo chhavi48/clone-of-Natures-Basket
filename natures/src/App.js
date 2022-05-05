74a038a72560ca6e1b9e5110e727dacab1ba1e2e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-05 10:09:39 +0530

    blogs

--- natures/src/App.js
+++ natures/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Slider from './Components/Slider';
-import Cards from './Components/Cards';
-import Events from './Components/Events';
+import Blogs from './Components/Blogs';
+// import Slider from './Components/Slider';
+// import Cards from './Components/Cards';
+// import Events from './Components/Events';
 function App() {
   return (
     <div className="App">
     <h1>hello</h1>
-    <Slider/>
+    {/* <Slider/>
   <Cards/>
-  <Events/>
+  <Events/> */}
+  <Blogs/>
     </div>
   );
 }
---
+++ natures/src/Components/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+      <>
+    <div>Blogs</div>
+    
+    </>
+  )
+}
+
+export default Blogs
