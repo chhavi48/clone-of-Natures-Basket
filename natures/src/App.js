655cd2451bf6545e0e6bfb0a4957dc1d0c2fdfd9
yashtomar15 <tomaryash15@gmail.com>
2022-05-04 13:32:23 -1200

    navbar

--- natures/src/App.js
+++ natures/src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import Slider from './Components/Slider';
-import Cards from './Components/Cards';
 
 function App() {
   return (
     <div className="App">
     <h1>hello</h1>
-    <Slider/>
-  <Cards/>
     </div>
   );
 }
