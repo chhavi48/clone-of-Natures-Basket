b8b4663f5cfa6a9df92418db11ccc8255e3c3d83
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-08 20:58:01 +0530

    cart

--- natures/src/Components/Addtocart.jsx
+++ natures/src/Components/Addtocart.jsx
@@ -4,10 +4,20 @@ const Addtocart = () => {
   return (
       <>
       <div>Addtocart</div>
-      <div className='container' style={{width:"700px",height:"500px" ,border:"2px solid black",marginLeft:"43%"}}>
-      <div className='first' style={{height:"300px",}}>
+      <div className='container' style={{width:"700px",height:"500px" ,border:"2px solid black",marginLeft:"43%",backgroundColor:"yellow"}}>
+      <div className='first' style={{height:"230px",}}>
         
 
+      </div>
+      <div className='second' style={{height:"250px",backgroundColor:"red"}}>
+    <p>HAND-PICKED OFFERS JUST FOR YOU</p>
+          <div className='one' style={{display:"flex"}}>
+            
+              <div className='two'>
+                 
+          </div>
+          </div>
+         
       </div>
       </div>
       </>
