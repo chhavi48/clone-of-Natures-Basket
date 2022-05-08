cc63219e99a83cd56cadeefb7c83f10f165d9f6c
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-05-08 21:07:17 +0530

    cart

--- natures/src/Components/Addtocart.jsx
+++ natures/src/Components/Addtocart.jsx
@@ -12,9 +12,11 @@ const Addtocart = () => {
       <div className='second' style={{height:"250px",backgroundColor:"red"}}>
     <p>HAND-PICKED OFFERS JUST FOR YOU</p>
           <div className='one' style={{display:"flex"}}>
-                 <div className='cart'>
+                 <div className='cart' style={{display:"flex"}}>
                      <img src="https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/b2a10a60-7a17-409d-b069-4b8440d1a5be_425x425.jpg" style={{width:"100px"}}/>
-                   
+                     <p>PERSIST DATA 500g SALAD</p><br/>
+                     <p>1pc</p><br/>
+                     <p>172</p>
                  </div>
               <div className='two'>
                 hlo 
